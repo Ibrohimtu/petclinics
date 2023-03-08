b2064af1f30ec005bab52f2176a06013d728c81d
ibrohim <turdaliyevibrohim35@gmail.com>
2023-03-08 13:56:49 +0500

    Pet-clinic

---
+++ src/main/java/uz/brogrammers/PetClinicApplicationTest.java
@@ -0,0 +1,11 @@
+package uz.brogrammers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplicationTest {
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApplicationTest.class, args);
+    }
+}
